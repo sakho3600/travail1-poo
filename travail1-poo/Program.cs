57f7cbe58551d54fbe0acba97774f0c9c1945fc8
Thierry Frycia <thierry@frycia.be>
2017-10-20 12:42:47 +0200

    Create C# project

---
+++ travail1-poo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace gestion{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string text = System.IO.File.ReadAllText(@"data.json");
+			Console.WriteLine(text);
+		}
+	}   
+}
