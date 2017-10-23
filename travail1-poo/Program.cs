86f3e91a37f5133805a603d5b8f0b0239cfe58d8
Julien15 <Julien15>
2017-10-23 14:46:20 +0200

    Add Data translate

--- travail1-poo/Program.cs
+++ travail1-poo/Program.cs
@@ -8,31 +8,72 @@ namespace gestion
 {
 
 
-    public class RootObject
+    public class DataEmploye
     {   public string ID { get; set; }
         public string Name { get; set; }
         public string Date { get; set; }
         public float Salaire { get; set; }
 
     }
+    public class DataClient
+    {
+        public string Name { get; set; }
+        public string DateStart { get; set; }
+        public string DateEnd { get; set; }
+    }
 
     class MainClass
     {
         public static void Main(string[] args)
-        {
-            string data = System.IO.File.ReadAllText(@"dataemployé.json");
-            Traduction(data);
+        {   
+            Dictionary<string, string> Ad = Traduction(LectureName(),LectureAdress());
+            foreach (KeyValuePair<string, string> kvp in Ad)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
             Console.ReadKey();
         }
 
-        static void Traduction(string data)
+        static List<string> LectureName()
         {
-            var h = JsonConvert.DeserializeObject<List<RootObject>>(data);
-            int count = h.Count;
-            for(int i=0;i<count;i++)
+            List<string> ListName = new List<string>();
+            string data = System.IO.File.ReadAllText(@"dataemployé.json");
+            var h = JsonConvert.DeserializeObject<List<DataEmploye>>(data);
+            int cont = h.Count;
+            for (int i = 0; i < cont; i++)
             {
-                Employé h[i].ID = new Employé(h[i].Name, h[i].Date, h[i].Salaire);
+                ListName.Add(h[i].Name);
             }
+            return ListName;
+        }
+
+        static List<string> LectureAdress()
+        {
+            List<string> ListAdress = new List<string>();
+            string AdressList = System.IO.File.ReadAllText(@"Dataadress.json");
+            var p = JsonConvert.DeserializeObject<List<string>>(AdressList);
+            int cont1 = p.Count;
+            for (int i = 0; i < cont1; i++)
+            {
+                ListAdress.Add(p[i]);
+            }
+            return ListAdress;
+
+        }
+
+        static Dictionary<string,string>Traduction(List<string> ListName, List<string> ListAdress)
+        {
+            string a = System.IO.File.ReadAllText(@"Dataadress.json");
+            var b = JsonConvert.DeserializeObject<List<string>>(a);
+            int cont1 = b.Count;
+            Dictionary<string, string> Adresse = new Dictionary<string, string>();
+            for(int i = 0; i < cont1; i++)
+            {
+                Adresse.Add(ListName[i], ListAdress[i]);
+            }
+            return Adresse;
+            
         }
     }
     
