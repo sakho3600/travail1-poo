19934747744c77724c838b1d2de8ae8c420f054d
Julien15 <Julien15>
2017-10-26 22:59:41 +0200

    Developement de l'interface

--- travail1-poo/Affichage.cs
+++ travail1-poo/Affichage.cs
@@ -10,6 +10,12 @@ namespace travail1poo
     {
         public static void Affiche(Entreprise societe)
         {
+            Menu(societe);
+            
+        }
+
+        public static void Menu(Entreprise societe)
+        {
             Console.WriteLine("Gestionnaire d'employés.\r\nVeuillez choisir un rapport à générer:\r\n");
             Console.WriteLine("1. Générer un rapport pour le manager.");
             Console.WriteLine("2. Générer un rapport pour le directeur des resources humaines.");
@@ -31,39 +37,70 @@ namespace travail1poo
             {
                 Console.WriteLine("Veuillez entrer un chiffre entre 1 et 3.");
             }
-
             switch (choice)
             {
                 case 1:
-                    Console.WriteLine("Choisissez le manager pour lequel le rapport doit être généré.");
-                    string response = Console.ReadLine();
-                    Console.WriteLine(societe.SetListConsultant(response));
+                    cas1(societe);
                     break;
                 case 2:
-                    Console.WriteLine("Choisissez l'entreprise dont vous voulez voir la liste de nos consultant qui y ont travaillé.");
-                    string reponse = Console.ReadLine();
-                    Console.WriteLine(societe.SetListEntreprise(reponse));
+                    cas2(societe);
                     break;
                 case 3:
-                    int choise = 0;
-                    try
-                    {
-                        string year = Console.ReadLine();
-                        choise = int.Parse(year);
-
-                        if ((2018>= choice) || (choice >= 2015))
-                        {
-                            throw new System.Exception();
-                        }
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Veuillez entrer une année entre 2016 et 2017");
-                    }
-                    Console.WriteLine(societe.SetListSalaire(societe.DirecteurFinance(),choise));
+                    cas3(societe);
                     break;
             }
+            retour(societe);
         }
+
+        public static void cas1(Entreprise societe)
+        {
+            Console.Clear();
+            Console.WriteLine("Choisissez le manager pour lequel le rapport doit être généré:");
+            string response1 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(societe.SetListConsultant(response1));
+        }
+
+        public static void cas2(Entreprise societe)
+        {
+            Console.Clear();
+            Console.WriteLine("Choisissez l'entreprise dont vous voulez voir la liste de nos consultant qui y ont travaillé:");
+            string response2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(societe.SetListEntreprise(response2));
+        }
+
+        public static void cas3(Entreprise societe)
+        {
+            Console.Clear();
+            Console.WriteLine("Choisissez une Année:");
+            string response3 = Console.ReadLine();
+            int choise = 0;
+            choise = Int16.Parse(response3);
+            Console.Clear();
+            Console.WriteLine(societe.SetListSalaire(societe.DirecteurFinance(), choise));
+        }
+        public static void retour(Entreprise societe)
+        {
+            Console.WriteLine("Voulez vous quittez l'interface ? (y/n)");
+            string response4 = Console.ReadLine();
+            if (response4 == "n")
+            {
+                Console.Clear();
+                Menu(societe);
+            }
+            else if (response4 == "y")
+            {
+                Console.Clear();
+                Console.WriteLine("Merci de votre visite !");
+            }
+            else
+            {
+                Console.WriteLine("Vous n'avez pas effectué la bonne commande");
+                retour(societe);
+            }
+        }
+
     }
     
 }
